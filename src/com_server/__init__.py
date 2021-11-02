da1ff3abac609a2c3af863078e6a299872dc25e5
Jonathan Liu <jonathanhliu21@gmail.com>
2021-11-01 22:08:35 -0700

    update __init__

--- src/com_server/__init__.py
+++ src/com_server/__init__.py
@@ -7,5 +7,7 @@ import os
 if (os.name != "posix" and os.name != "nt"):
     raise EnvironmentError("Platform not supported.")
 
+from .api_server import *
+from .base_connection import *
 from .connection import *
 from . import *
