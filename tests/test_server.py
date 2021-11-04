07f3d5cc96678c4152af3c295b516a97527230e8
Jonathan Liu <jonathanhliu21@gmail.com>
2021-11-03 22:00:40 -0700

    add main test

--- tests/test_server.py
+++ tests/test_server.py
@@ -54,3 +54,20 @@ def test_subclass_type_exception():
     
     ex = e.value
     assert isinstance(ex, TypeError)
+
+if (__name__ == "__main__"):
+    # pytest should not run this
+
+    conn = com_server.Connection(115200, "/dev/ttyUSB0")
+    handler = com_server.RestApiHandler(conn)
+
+    @handler.add_endpoint("/hello_world")
+    def hello_world(conn):
+        class Hello_World(com_server.ConnectionResource):
+            def post(self):
+                return {"hello": "world"}
+    
+        return Hello_World
+    
+    handler.run(host='0.0.0.0', port=8080)
+    
