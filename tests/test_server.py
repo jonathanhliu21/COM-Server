a49834f8d90cd03b6949d44b9ea52793f87f620a
Jonathan Liu <jonathanhliu21@gmail.com>
2021-11-03 21:40:04 -0700

    update tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -7,27 +7,50 @@ import com_server
 import pytest
 from flask_restful import Resource
 
+def test_endpointexistsexception():
+    """
+    Tests if EndpointExistsException is being thrown
+    """
 
-@pytest.mark.skip(reason="No way of testing without infinite loop")
-def test_server_start():
     conn = com_server.Connection(115200, "/dev/ttyUSB0")
-    serve = com_server.Base_Rest_Connection(conn)
+    handler = com_server.RestApiHandler(conn)
 
-    @serve.add_endpoint("/test")
-    def endpt(obj):
-        class TestEndpt(Resource):
-            def get(self):
-                obj.send(f"sent: {time.time()}")
-                a = obj.get(str)
-                print(a)
-                return {"test": "OK", "rcv": a}
+    @handler.add_endpoint("/test1")
+    def test1(conn):
+        class Test1(com_server.ConnectionResource):
+            pass
 
-        print(isinstance(TestEndpt, Resource))
+        return Test1
 
-        return TestEndpt
+    # should raise EndpointExistsException
+    with pytest.raises(com_server.api_server.EndpointExistsException) as e:
+        @handler.add_endpoint("/test1")
+        def test2(conn):
+            class Test1(com_server.ConnectionResource):
+                pass
 
-    serve.run(host="0.0.0.0", port=8080)
+            return Test1
 
+    ex = e.value
+    assert isinstance(ex, com_server.api_server.EndpointExistsException)
+    
 
-if (__name__ == "__main__"):
-    test_server_start()
+def test_subclass_type_exception():
+    """
+    Tests if exception is being thrown if subclass extends wrong type
+    """
+
+    conn = com_server.Connection(115200, "/dev/ttyUSB0")
+    handler = com_server.RestApiHandler(conn)
+
+    # should raise TypeError
+    with pytest.raises(TypeError) as e:
+        @handler.add_endpoint("/test2")
+        def test2(conn):
+            class Test1(Resource):
+                pass
+
+            return Test1
+    
+    ex = e.value
+    assert isinstance(ex, TypeError)
