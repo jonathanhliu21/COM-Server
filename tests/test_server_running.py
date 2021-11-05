a41049e05eb2c0ada2a3f98609bd2a9bc0d89c8a
Jonathan Liu <jonathanhliu21@gmail.com>
2021-11-05 16:55:28 -0700

    update tests

--- tests/test_server_running.py
+++ tests/test_server_running.py
@@ -7,6 +7,7 @@ Make sure server is running on local computer with host "0.0.0.0" and port "8080
 """
 
 import os
+import sys
 
 import pytest
 import requests
@@ -16,15 +17,17 @@ from com_server import (BaseConnection, Connection, ConnectionResource,
 SERVER = "http://0.0.0.0:8080"
 
 try:
-    requests.get(SERVER)
+    requests.get(SERVER+"/recall")
 except requests.exceptions.ConnectionError:
     pytestmark = pytest.mark.skip(reason="Server not launched. Make sure it is running on 0.0.0.0 with port 8080, or run \"python3 tests/start_server.py\".")
-
-requests.get(SERVER+"/recall")
 
 def test_register() -> None:
     r = requests.get(SERVER + "/register")
     assert r.status_code == 200
+
+def test_register_after_registered() -> None:
+    r = requests.get(SERVER + "/register")
+    assert r.status_code == 400
 
 def test_unregister() -> None:
     r = requests.get(SERVER + "/recall")
