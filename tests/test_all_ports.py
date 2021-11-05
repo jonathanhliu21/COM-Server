b553e78e5062b4faa3514faf9e27cebda108e7ee
Jonathan Liu <jonathanhliu21@gmail.com>
2021-11-04 20:01:39 -0700

    update tests

--- tests/test_all_ports.py
+++ tests/test_all_ports.py
@@ -2,14 +2,22 @@
 # -*- coding: utf-8 -*-
 
 """
-Tests if lists ports properly
+Tests if lists ports properly. Needs Arduino microcontroller to be plugged in to test (otherwise skipped).
 """
 
+import glob
+import sys
 import re
 
 from com_server.disconnect import _get_all_ports
 from com_server.tools import all_ports
 
+import pytest
+
+_usb = glob.glob("/dev/ttyUSB[0-9]*")
+_acm = glob.glob("/dev/ttyACM[0-9]*")
+
+@pytest.mark.skipif(not sys.platform.startswith("linux"), len(_usb+_acm) <= 0, reason="port not connected")
 def test_ports():
     """
     Tests if `all_ports` is listing properly.    
@@ -19,6 +27,7 @@ def test_ports():
 
     assert(len(ports) > 0)
 
+@pytest.mark.skipif(not sys.platform.startswith("linux"), len(_usb+_acm) <= 0, reason="port not connected")
 def test_get_all():
     """
     Tests if `_get_all_ports()` is listing properly.
--- tests/test_server.py
+++ tests/test_server.py
@@ -7,7 +7,7 @@ import com_server
 import pytest
 from flask_restful import Resource
 
-def test_endpointexistsexception():
+def test_endpointexistsexception() -> None:
     """
     Tests if EndpointExistsException is being thrown
     """
@@ -35,7 +35,7 @@ def test_endpointexistsexception():
     assert isinstance(ex, com_server.api_server.EndpointExistsException)
     
 
-def test_subclass_type_exception():
+def test_subclass_type_exception() -> None:
     """
     Tests if exception is being thrown if subclass extends wrong type
     """
@@ -55,6 +55,64 @@ def test_subclass_type_exception():
     ex = e.value
     assert isinstance(ex, TypeError)
 
+def test_duplicate_class_no_exception() -> None:
+    """
+    Tests if duplicate class names lead to exceptions
+    """
+    
+    conn = com_server.Connection(115200, "/dev/ttyUSB0")
+    handler = com_server.RestApiHandler(conn)
+
+    @handler.add_endpoint("/hello_world")
+    def hello_world_test(conn):
+        class Hello_World_(com_server.ConnectionResource):
+            def get(self):
+                return {"hello": "world"}
+    
+        return Hello_World_
+    
+    @handler.add_endpoint("/hello_world_2")
+    def hello_world_2_test(conn):
+        class Hello_World_(com_server.ConnectionResource):
+            def get(self):
+                return {"hello": "world2"}
+        
+        return Hello_World_
+    
+    for e, r in handler.all_endpoints: 
+        handler.api.add_resource(r, e)
+    
+    assert len(handler.all_endpoints) == 2
+
+def test_duplicate_func_name_no_exception() -> None:
+    """
+    Tests if functions of the same name will lead to exceptions
+    """
+
+    conn = com_server.Connection(115200, "/dev/ttyUSB0")
+    handler = com_server.RestApiHandler(conn)
+
+    @handler.add_endpoint("/hello_world")
+    def hello_world_test(conn):
+        class Hello_World_(com_server.ConnectionResource):
+            def get(self):
+                return {"hello": "world"}
+    
+        return Hello_World_
+    
+    @handler.add_endpoint("/hello_world_2")
+    def hello_world_test(conn):
+        class Hello_World_(com_server.ConnectionResource):
+            def get(self):
+                return {"hello": "world2"}
+        
+        return Hello_World_
+    
+    for e, r in handler.all_endpoints: 
+        handler.api.add_resource(r, e)
+    
+    assert len(handler.all_endpoints) == 2
+
 if (__name__ == "__main__"):
     # pytest should not run this
 
@@ -62,12 +120,20 @@ if (__name__ == "__main__"):
     handler = com_server.RestApiHandler(conn)
 
     @handler.add_endpoint("/hello_world")
-    def hello_world(conn):
-        class Hello_World(com_server.ConnectionResource):
-            def post(self):
+    def hello_world_test(conn):
+        class Hello_World_(com_server.ConnectionResource):
+            def get(self):
                 return {"hello": "world"}
     
-        return Hello_World
+        return Hello_World_
+    
+    @handler.add_endpoint("/hello_world_2")
+    def hello_world_2_test(conn):
+        class Hello_World_(com_server.ConnectionResource):
+            def get(self):
+                return {"hello": "world2"}
+        
+        return Hello_World_
     
     handler.run(host='0.0.0.0', port=8080)
     
