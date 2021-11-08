addbed031dfbe8f594b3a191ff4c3917e26ce8c4
Jonathan Liu <jonathanhliu21@gmail.com>
2021-11-07 21:11:14 -0800

    make env default to dev

--- src/com_server/__main__.py
+++ src/com_server/__main__.py
@@ -20,14 +20,14 @@ with the serial port in an development environment or a
 production environment.
 
 Usage:
-    com_server (-p | --serport) <serport> (-b | --baud) <baud> run --env=<env> [--host=<host>] [--port=<port>] [--s-int=<s-int>] [--to=<to>]
+    com_server (-p | --serport) <serport> (-b | --baud) <baud> run [--env=<env>] [--host=<host>] [--port=<port>] [--s-int=<s-int>] [--to=<to>]
     com_server -h | --help
     com_server --version
 
 Options:
     -p, --serport    The serial port to connect to.
     -b, --baud       The baud rate of the serial connection.
-    --env=<env>      Development or production environment. Value must be 'dev' or 'prod'.
+    --env=<env>      Development or production environment. Value must be 'dev' or 'prod'. [default: dev].
     --host=<host>    The name of the host server (optional) [default: 0.0.0.0].
     --port=<port>    The port of the host server (optional) [default: 8080].
     --s-int=<s-int>  How long, in seconds, the program should wait between sending to serial port [default: 1].
