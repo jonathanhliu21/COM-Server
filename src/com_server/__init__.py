293d819aadfbe647f62e6b97fbebec71e27d13a4
Jonathan Liu <jonathanhliu21@gmail.com>
2021-11-09 20:26:59 -0800

    add endpointExistsException to __init__

--- src/com_server/__init__.py
+++ src/com_server/__init__.py
@@ -13,7 +13,7 @@ vers = sys.version_info
 if (vers.major < 3 or (vers.major == 3 and vers.minor < 6)):
     raise EnvironmentError("Python version >= 3.6 is required")
 
-from .api_server import ConnectionResource, RestApiHandler
+from .api_server import ConnectionResource, RestApiHandler, EndpointExistsException
 from .base_connection import BaseConnection, ConnectException
 from .connection import Connection
 from .tools import all_ports
