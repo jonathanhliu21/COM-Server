f1c21401642a46b41dab720b3ae9b0bcee9942a3
Jonathan Liu <jonathanhliu21@gmail.com>
2021-11-11 21:19:10 -0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     install_requires=[
         "setuptools>=58.5.3",
-        "wheel>=0.37.0"
+        "wheel>=0.37.0",
         "pyserial>=3.5",
         "flask_restful>=0.3.9",
         "docopt>=0.6.2",
