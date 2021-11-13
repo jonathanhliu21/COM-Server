c01df6c0897dee2e62887f199fea9d2123edcb88
Jonathan Liu <jonathanhliu21@gmail.com>
2021-11-13 08:49:40 -0800

    add import to constants

--- src/com_server/__init__.py
+++ src/com_server/__init__.py
@@ -14,6 +14,7 @@ if (vers.major < 3 or (vers.major == 3 and vers.minor < 6)):
     raise EnvironmentError("Python version >= 3.6 is required")
 
 from .api_server import ConnectionResource, RestApiHandler, EndpointExistsException
+from .constants import *
 from .base_connection import BaseConnection, ConnectException
 from .connection import Connection
 from .tools import all_ports
