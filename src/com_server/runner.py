443571345db02a806bafa44d6875f83efc6541be
Jonathan Liu <jonathanhliu21@gmail.com>
2021-11-16 21:36:39 -0800

    add more verbose output

--- src/com_server/runner.py
+++ src/com_server/runner.py
@@ -9,11 +9,17 @@ from . import Connection, RestApiHandler, Builtins
 
 def run(baud: int, ser_port: int, env: str, host: str, port: str, timeout: int, send_interval: int):
     # init connection
-    conn = Connection(baud, ser_port, timeout=timeout, send_interval=send_interval) 
-    handler = RestApiHandler(conn)
-    Builtins(handler)
 
-    if (env == "dev"):
-        handler.run_dev(host=host, port=port)
-    else:
-        handler.run_prod(host=host, port=port)
+    print("Starting up connection with serial port...")
+    with Connection(baud, ser_port, timeout=timeout, send_interval=send_interval) as conn:
+        print("Connection with serial port established")
+
+        handler = RestApiHandler(conn)
+        Builtins(handler)
+
+        if (env == "dev"):
+            print("Launching Flask app...")
+            handler.run_dev(host=host, port=port)
+        else:
+            print("Launching Waitress server...")
+            handler.run_prod(host=host, port=port)
