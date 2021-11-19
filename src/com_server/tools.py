e89ded31e040d86c239e04d0cad3ac35684633ec
Jonathan Liu <jonathanhliu21@gmail.com>
2021-11-18 22:04:33 -0800

    implement receive queue

--- src/com_server/tools.py
+++ src/com_server/tools.py
@@ -6,6 +6,7 @@ Provides a set of functions that could be generally useful.
 """
 
 import copy
+import time
 import typing as t
 
 import serial
@@ -30,7 +31,7 @@ class SendQueue:
     the `send()` method. 
 
     Makes sure the user only reads and pops from send queue
-    and does not add or delete anything from the queue.
+    and does not directly add or delete anything from the queue.
     """
 
     def __init__(self, send_queue: list) -> None:
@@ -99,13 +100,13 @@ class SendQueue:
         - None
 
         Returns:
-        - A shallow copy of the list.
+        - A shallow copy of the send queue
         """
     
         return self._send_queue.copy()
 
     def deepcopy(self) -> list:
-        """Returns a deepcopy of the list.
+        """Returns a deepcopy of the send queue list.
 
         By using this, you can modify the list without altering
         any elements of the actual send queue itself. However,
@@ -115,7 +116,103 @@ class SendQueue:
         - None
 
         Returns:
-        - A deep copy of the list
+        - A deep copy of the send queue
         """
 
         return copy.deepcopy(self._send_queue)
+
+class ReceiveQueue:
+    """The ReceiveQueue object.
+    
+    This object is a queue, but the user can 
+    only add bytes object(s) to it. 
+
+    Makes sure the user does not directly add,
+    delete, or modify the queue. 
+    """
+
+    def __init__(self, rcv_queue: list, queue_size: int) -> None:
+        """Constructor for send queue object.
+
+        Parameters:
+        - `rcv_queue` (list): The list that will act as the receive queue.
+        - `queue_size` (int): The maximum size of the receive queue
+
+        Returns:
+        - Nothing 
+        """
+
+        self._rcv_queue = rcv_queue
+        self._queue_size = queue_size
+    
+    def __len__(self) -> int:
+        """
+        Returns the length of the receive queue.
+        """
+
+        return len(self._rcv_queue)
+    
+    def __repr__(self) -> str:
+        """
+        String representation of queue.
+        """
+
+        return f"ReceiveQueue{self._rcv_queue}"
+    
+    def additems(self, *args) -> None:
+        """Adds a list of items to the receive queue.
+
+        All items in `*args` must be a `bytes` object. A
+        `TypeError` will be raised if not.
+
+        If the size exceeds `queue_size` when adding, then
+        it will pop the front of the queue. 
+
+        A tuple (timestamp, bytes) will be added. The timestamp
+        will be regenerated for each iteration of the for loop
+        so they will be in order when binary searching.
+        """
+
+        for obj in args:
+            if (not isinstance(obj, bytes)):
+                raise TypeError("Every argument must be a bytes object")
+            
+            # add timestamp, obj to queue
+            self._rcv_queue.append((time.time(), obj))
+
+            if (len(self._rcv_queue) > self._queue_size):
+                # if greater than queue size, then pop first element
+                self._rcv_queue.pop(0)
+
+    def copy(self) -> list:
+        """Returns a shallow copy of the receive queue list. 
+
+        Using this to copy to a list may be dangerous, as
+        altering elements in the list may alter the elements
+        in the receive queue itself. To prevent this, use the
+        `deepcopy()` method.
+
+        Parameters:
+        - None
+
+        Returns:
+        - A shallow copy of the receive queue
+        """
+    
+        return self._rcv_queue.copy()
+
+    def deepcopy(self) -> list:
+        """Returns a deepcopy of the receive queue.
+
+        By using this, you can modify the list without altering
+        any elements of the actual send queue itself. However,
+        it is a little more resource intensive.
+
+        Parameters:
+        - None
+
+        Returns:
+        - A deep copy of the receive queue
+        """
+
+        return copy.deepcopy(self._rcv_queue)
