4643445065505cd6e36bf783b69821ea13bb95f8
Jonathan Liu <jonathanhliu21@gmail.com>
2021-11-19 18:04:23 -0800

    add baud rates

--- src/com_server/constants.py
+++ src/com_server/constants.py
@@ -5,9 +5,15 @@
 A file containing constants for COM-Server.
 """
 
+# timeouts & send intervals
 NO_TIMEOUT = float("inf")
 NO_SEND_INTERVAL = 0
 
+# serial baud rates
+NORMAL_BAUD_RATE = 9600
+FAST_BAUD_RATE = 115200
+
+# receive queue
 NO_RCV_QUEUE = 1 # not 0 because the program would then disregard all incoming data
 RCV_QUEUE_SIZE_XSMALL = 32
 RCV_QUEUE_SIZE_SMALL = 128
@@ -15,5 +21,6 @@ RCV_QUEUE_SIZE_NORMAL = 256
 RCV_QUEUE_SIZE_LARGE = 512
 RCV_QUEUE_SIZE_XLARGE = 1024
 
+# server
 DEFAULT_HOST="0.0.0.0"
 DEFAULT_PORT=8080
