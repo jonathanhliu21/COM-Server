1d22bc653bfad5d36fbbb1ecc0fe574079fd5937
Jonathan Liu <jonathanhliu21@gmail.com>
2021-12-19 22:09:11 -0800

    add tests

--- tests/methods/test_connections.py
+++ tests/methods/test_connections.py
@@ -6,6 +6,7 @@ Tests the connection endpoints such as connected() and list_ports()
 """
 
 import json
+import time
 
 from com_server import all_ports
 import pytest
@@ -52,6 +53,38 @@ def test_list_ports():
 
     assert r.status_code == 200
 
+def test_available():
+    """
+    Tests available property
+    """
+
+    requests.get(SERVER+"/receive")
+
+    data = {
+        "data": [1, 2, 3, 4],
+        "ending": "\n",
+        "concatenate": ";"
+    } 
+    requests.post(SERVER+"/send", data=data)
+
+    time.sleep(1) # for send interval
+
+    r = requests.get(SERVER+"/connection_state")
+    loaded = json.loads(r.text)
+    state = loaded["state"]
+
+    assert state["available"] == 1
+
+def test_timeout_sendint():
+    """
+    Tests that timeout and send interval are 1.0
+    """
+
+    r = requests.get(SERVER+"/connection_state")
+    loaded = json.loads(r.text)
+    state = loaded["state"]
+
+    assert state["send_interval"] == 1.0 and state["timeout"] == 1.0
 
 def test_unregister() -> None:
     r = requests.get(SERVER + "/recall")
