b6882a88027db3ff330fe5bfcc8a8bc8b7c3b4ba
Jonathan Liu <jonathanhliu21@gmail.com>
2021-12-24 18:28:04 -0800

    add queue size

--- src/com_server/runner.py
+++ src/com_server/runner.py
@@ -16,6 +16,7 @@ def run(
     port: str,
     timeout: int,
     send_interval: int,
+    queue_size: int,
     cors: bool,
     has_rr: bool,
     verbose: bool,
@@ -24,7 +25,12 @@ def run(
 
     print("Starting up connection with serial port...")
     with Connection(
-        baud, ser_port[0], *ser_port[1:], timeout=timeout, send_interval=send_interval
+        baud,
+        ser_port[0],
+        *ser_port[1:],
+        timeout=timeout,
+        send_interval=send_interval,
+        queue_size=queue_size
     ) as conn:
         print("Connection with serial port established at", conn.port)
 
