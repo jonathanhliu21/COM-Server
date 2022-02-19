2d868b8c6115fe6279b7a638f9624330f4dfb436
Jonathan Liu <jonathanhliu21@gmail.com>
2022-02-18 21:50:17 -0800

    remove baseconnection

--- src/com_server/__init__.py
+++ src/com_server/__init__.py
@@ -14,7 +14,7 @@ if vers.major < 3 or (vers.major == 3 and vers.minor < 6):
     raise EnvironmentError("Python version >= 3.6 is required")
 
 from .api_server import ConnectionResource, EndpointExistsException, RestApiHandler
-from .base_connection import BaseConnection, ConnectException
+from .base_connection import ConnectException
 from .connection import Connection
 from .constants import *
 from .tools import ReceiveQueue, SendQueue, all_ports
