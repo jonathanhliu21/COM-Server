d824d353ce00adfc3e4180267f800d899b8e68e6
Jonathan Liu <jonathanhliu21@gmail.com>
2022-02-18 21:49:19 -0800

    add new server design

--- src/com_server/disconnect.py
+++ src/com_server/disconnect.py
@@ -10,17 +10,41 @@ import threading
 import time
 import typing as t
 
-from . import connection  # for typing
+from .connection import Connection
 
 
-class Reconnector(threading.Thread):
+class BaseReconnector(threading.Thread):
+    def _init_logger(self) -> None:
+        """Initializes logger to stdout"""
+
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.INFO)
+
+        fmt = logging.Formatter("%(levelname)s [%(asctime)s] - %(message)s")
+        handler.setFormatter(fmt)
+
+        self._logger.addHandler(handler)
+
+    def _init_logger_file(self) -> None:
+        """Initializes logger to file"""
+
+        handler = logging.FileHandler(self._logf)
+        handler.setLevel(logging.INFO)
+
+        fmt = logging.Formatter("%(levelname)s [%(asctime)s] - %(message)s")
+        handler.setFormatter(fmt)
+
+        self._logger.addHandler(handler)
+
+
+class Reconnector(BaseReconnector):
     """
-    Object that detects whenever a connection is disconnected and reconnects
+    Object that detects whenever a single connection is disconnected and reconnects
     """
 
     def __init__(
         self,
-        conn: connection.Connection,
+        conn: Connection,
         logger: logging.Logger,
         logfile: t.Optional[str] = None,
     ) -> None:
@@ -32,7 +56,7 @@ class Reconnector(threading.Thread):
 
         Arguments:
         - `conn` (Connection): connection to watch and reconnect to.
-        - `prod` (bool): indicates if this is a production server
+        - `logger` (Logger): logger object
         - `logfile` (str, None): the path to the file to log disconnects to
         """
 
@@ -69,24 +93,77 @@ class Reconnector(threading.Thread):
 
             time.sleep(0.01)
 
-    def _init_logger(self) -> None:
-        """Initializes logger to stdout"""
 
-        handler = logging.StreamHandler()
-        handler.setLevel(logging.INFO)
+class MultiReconnector(BaseReconnector):
+    """
+    Object that detects whenever any given connection is disconnected and reconnects
+    """
 
-        fmt = logging.Formatter("%(levelname)s [%(asctime)s] - %(message)s")
-        handler.setFormatter(fmt)
+    def __init__(
+        self,
+        logger: logging.Logger,
+        *conns: Connection,
+        logfile: t.Optional[str] = None,
+    ) -> None:
+        """
+        Takes in the connection objects to watch and reconnect if any are disconnected.
+        This is NOT thread safe and not meant to be used outside of other connection objects.
+        You should implement your own disconnect/reconnect handler.
 
-        self._logger.addHandler(handler)
+        Arguments:
+        - `logger` (Logger): logger object
+        - `*conns` (Connection): connections to watch and reconnect to.
+        - `logfile` (str, None): the path to the file to log disconnects to
+        """
 
-    def _init_logger_file(self) -> None:
-        """Initializes logger to file"""
+        self._conns = conns
+        self._logf = logfile
 
-        handler = logging.FileHandler(self._logf)
-        handler.setLevel(logging.INFO)
+        self._logger = logger
+        self._logger.propagate = (
+            False  # prevents from logging twice because waitress calls basicConfig()
+        )
+        self._logger.setLevel(logging.INFO)
+        self._init_logger()
 
-        fmt = logging.Formatter("%(levelname)s [%(asctime)s] - %(message)s")
-        handler.setFormatter(fmt)
+        if self._logf:
+            self._init_logger_file()
 
-        self._logger.addHandler(handler)
+        # threading
+        super().__init__(daemon=True)
+
+    def run(self) -> None:
+        """What to run in thread
+
+        In this case, checks if the serial port every 0.01 seconds.
+        """
+
+        cur_reconn = set()
+
+        def _reconn(conn: Connection) -> None:
+            """
+            reconnect thread
+            """
+
+            if conn in cur_reconn:
+                # if currently reconnecting, then exit
+                return
+
+            cur_reconn.add(conn)
+            conn.reconnect()
+
+            # remove of set of currently reconnecting objects after reconnected
+            cur_reconn.remove(conn)
+
+        while True:
+            for conn in self._conns:
+                if not conn.connected:
+
+                    self._logger.warning(f"Device at {conn.port} disconnected")
+                    self._logger.info("Attempting to reconnect...")
+
+                    threading.Thread(target=_reconn, args=(conn,), daemon=True).start()
+
+                    self._logger.info(f"Device reconnected at {conn.port}")
+
+            time.sleep(0.01)
