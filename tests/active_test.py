ed407a68dff9239aac78982b6c00e736266dc428
Jonathan Liu <jonathanhliu21@gmail.com>
2022-02-21 15:51:38 -0800

    add check for server

--- tests/active_test.py
+++ tests/active_test.py
@@ -7,6 +7,13 @@ Script to run tests that need a serial connection to run
 
 import os
 import pytest
+import requests
 import sys
+
+try:
+    requests.get("http://127.0.0.1:8080")
+except requests.exceptions.ConnectionError:
+    print("Server not found. Skipping tests and exiting.")
+    sys.exit()
 
 pytest.main([os.path.join("tests", "active")] + sys.argv[1:])
