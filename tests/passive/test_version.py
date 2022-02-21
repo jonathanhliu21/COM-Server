2ec794a886e26835bb3431ee2c70116aa471453f
Jonathan Liu <jonathanhliu21@gmail.com>
2022-02-21 09:52:34 -0800

    fix cmp_version

--- tests/passive/test_version.py
+++ tests/passive/test_version.py
@@ -9,7 +9,7 @@ import pytest
 from com_server import __version__
 from pkg_resources import get_distribution
 
-from cmp_version import Version
+from tests.cmp_version import Version
 
 
 def test_version():
